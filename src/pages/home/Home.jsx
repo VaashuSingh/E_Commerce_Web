8826b7a670de911a3b9709e604cb38822ddc66bf
Raja Singh <rajaisexcellent@gmail.com>
2024-07-09 23:38:50 +0530

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../components/layout/Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum
+      consectetur doloremque sunt dignissimos quas sit adipisci, numquam maiores
+      neque animi facere magni voluptate. Eaque vitae explicabo reiciendis
+      soluta, animi facilis.
+    </Layout>
+  );
+};
+
+export default Home;
