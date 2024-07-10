582afe67e138be831e2ee522412581f74a0a42f5
Raja Singh <rajaisexcellent@gmail.com>
2024-07-10 18:02:00 +0530

    "Wrapped App component with MyState context provider and added useContext hook to Home component to consume context values"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // react-router-dom
 import Home from "./pages/home/Home";
+import MyState from "./context/data/myState";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <MyState>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </MyState>
   );
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { useContext } from "react";
 import Layout from "../../components/layout/Layout";
+import MyContext from "../../context/data/myContext";
 
 const Home = () => {
+  const context = useContext(MyContext);
+  const { name, roll } = context;
+  console.log("context", context);
   return (
     <Layout>
+      <h1>Name : {name}</h1>
+      <h1>Roll : {roll}</h1>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum
       consectetur doloremque sunt dignissimos quas sit adipisci, numquam maiores
       neque animi facere magni voluptate. Eaque vitae explicabo reiciendis
