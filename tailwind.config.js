74471676a909d04182efc44fa3e734a8204440ae
Raja Singh <rajaisexcellent@gmail.com>
2024-07-10 10:49:23 +0530

    "Update tailwind.config.js: change content paths and switch from default export to module.exports"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  // ...
+}
